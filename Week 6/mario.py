4425f0d9fb1e0cb7c7161a5115e58fac1339e7a9
Moritz Eckardt <moritz.eckardt@brz.eu>
2021-11-01 13:19:26 +0100

    Upload mario.py

---
+++ Week 6/mario.py
@@ -0,0 +1,26 @@
+from cs50 import get_int
+
+def main():
+    # Enter height & determine amounf of spaces
+    while True:
+        height = get_int("Height of pyramides (1 - 8): ")
+        if height > 0 and height < 9:
+            amount_spaces = height - 1
+            break
+    
+    # Build pyramide
+    for i in range(1, height + 1):
+        amount_hashes = i
+        amount_spaces = height - amount_hashes
+
+        print(" " * amount_spaces, end="")
+        
+        print("#" * amount_hashes, end="")
+
+        print("  ", end="")
+
+        print("#" * amount_hashes)
+
+
+if __name__ == "__main__":
+    main()   
